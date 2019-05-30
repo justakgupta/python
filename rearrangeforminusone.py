c7d2efdc1339562e5d4b576199225edc17d7b388
Amit Gupta <amitgupta.nitb@gmail.com>
2019-05-30 23:45:21 +0200

    rearrange an array with -1. updated. working

--- rearrangeforminusone.py
+++ rearrangeforminusone.py
@@ -1,5 +1,19 @@
 def rearrange(arr):
-	pass
+	#navigate the array if a[i] = -1 then leave
+	for i in range(len(arr)):
+		#print arr[i]
+		if(arr[i] != -1 and arr[i] != i):
+				x = arr[i]
+				while (arr[x] != -1 and arr[x] != x):
+					y = arr[x]
+					arr[x] = x
+					x = y
+				arr[x] = x
+				if (arr[i] != i):
+					arr[i] = -1	
+					pass
+	return arr
+		# pass
 
-inputarray = [-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]
-rearrange(inputarray)
+inputarray = [-1, -1, 9, 1, 9, 3, 2, -1, 4, -1]
+print rearrange(inputarray)
